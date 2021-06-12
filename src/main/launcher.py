7cd90f53e23c14a9b9e782f68165974114f6965c
A Crazy Town <47027981+ACrazyTown@users.noreply.github.com>
2021-06-12 16:11:26 +0200

    Create launcher.py

---
+++ src/main/launcher.py
@@ -0,0 +1,23 @@
+# legal mumbo jumbo
+
+# AscentViewer, a Python image viewer.
+# Copyright (C) 2020-2021 DespawnedDiamond, A Crazy Town and other contributors
+#
+# This file is part of AscentViewer.
+#
+# AscentViewer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AscentViewer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with AscentViewer.  If not, see <https://www.gnu.org/licenses/>.
+
+# =====================================================
+# Thank you for using and/or checking out AscentViewer!
+# =====================================================
